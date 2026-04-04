2df78a40c53514bd35078ece9bba51e9084701e2
xuanthai <xuanthai2305@gmail.com>
2026-04-04 11:27:32 +0700

    test

--- backend/tests/test_smartlogger.py
+++ backend/tests/test_smartlogger.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import time
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
@@ -26,13 +26,14 @@ def test_smartlogger():
     try:
         smartlogger = SmartLoggerHuawei(transport, slave_id=0)
         print("\n=== SMARTLOGGER CONTROL ===")
-        smartlogger.control_P(400)
+        smartlogger.control_P(300)
         print("Đã điều khiển công suất")
+        time.sleep(5)
 
         print("=== SMARTLOGGER STATUS ===")
         status = smartlogger.control_status()
         print(status)
-        
+        time.sleep(10)
 
         print("\n=== SMARTLOGGER ACTUAL POWER ===")
         actual_power = smartlogger.read_actual_power()
